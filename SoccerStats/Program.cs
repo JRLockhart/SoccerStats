9a19f87d3e37c070cb57c6359e3ec76602433ebc
James Lockhart <james.lockhart117@gmail.com>
2018-01-04 13:26:48 +0300

    Added serialization from a Json file

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace SoccerStats
 {
@@ -16,6 +17,13 @@ namespace SoccerStats
 
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
+            fileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(fileName);
+
+            foreach(var player in players)
+            {
+                
+            }
             Console.ReadLine();
 
         }
@@ -84,6 +92,39 @@ namespace SoccerStats
                 }  
             }
             return soccerResults;
+        }
+
+        public static List<Player> DeserializePlayers(string fileName)
+        {
+            var players = new List<Player>();
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(fileName))
+                using (var jsonReader = new JsonTextReader(reader))
+            {
+                serializer.Deserialize<List<Player>>(jsonReader);
+            }
+           
+            return players;
+        }
+
+        public static List<Player> GetTopTenPlayers(List<Player> players)
+        {
+            var topTenPlayers = new List<Player>();
+            players.Sort(new PlayerComparer());
+            int counter = 0;
+            foreach(var player in players)
+            {
+                topTenPlayers.Add(player);
+                counter++;
+                if (counter == 10)
+                    break;
+            }
+            return players;
+        }
+
+        public static void SerializePlayerToFile(List<Player> players, string fileName)
+        {
+
         }
     }
 }
